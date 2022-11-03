33d5c2f35b368c41e2c58ec92415735e8b898de3
ugodeughi <stefano.corradi@gmail.com>
2022-11-03 14:50:20 +0100

    v-if e v-for

--- js/script.js
+++ js/script.js
@@ -10,9 +10,32 @@ createApp({
       messaggio:'Ciao Vue',
       altroMessaggio: 'Buongiorno',
       miaClasse: 'verde',
+      altraClasse: 'sottolineato',
       mioTestoNonHtml: 'Questo è un testo NON HTML',
       mioTestoHtml: 'Questo è un testo <strong>HTML</strong>',
-      isTestoShow: true
+      isTestoShow: true,
+      sottolinea: true,
+      stampaConIf: 'secondo',
+      colori:['red','yellow', 'blue'],
+      limit: 10,
+      pizze:[
+        {
+          nome: 'Margherita',
+          prezzo: '€ 8:00',
+          ingredienti:['pomodoro', 'mozzarella']
+        },
+        {
+          nome: 'Marinara',
+          prezzo: '€ 7:00',
+          ingredienti:['pomodoro', 'acciughe']
+        },
+        {
+          nome: 'Diavola',
+          prezzo: '€ 9:00',
+          ingredienti:['pomodoro', 'mozzarella', 'nduja']
+        },
+      ],
+      clock:''
     }
   },
   methods:{
@@ -42,8 +65,47 @@ createApp({
       this.isTestoShow = mostraNascondi
     },
     toggleTesto(){
+      console.log(this.isTestoShow)
       this.isTestoShow = !this.isTestoShow;
+    },
+    toggleSottolinea(){
+      this.sottolinea = !this.sottolinea;
+      console.log(this.sottolinea);
+    },
+    handlerKeyUp(){
+      console.log(this.messaggio);
+    },
+    addRemoveBox(add){
+      if(this.limit > 0 && this.limit < 20){
+        add ?  this.limit++ : this.limit--;
+      }
+    },
+    getClock(){
+      const d = new Date();
+      this.clock =  `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`
+    },
+    startClock(){
+      // con setInterval dobbiamo usare l'arrow function per poter accedere al this di Vue
+      setInterval(() => {
+        this.getClock();
+      },1000)
     }
-  }
+  },
+  mounted(){
+    // azioni che vogliamo scatenare al caricamento dell'app
+    console.log('APP MONTATA')
+
+    // in console log vediamo il Porxy object ma lo utilizziamo come un oggetto normale
+    console.log(this.pizze)
+
+    // faccio stampare al caricamento del DOM l'orologio
+    this.getClock();
+    // al caricamento parte la funzione che gestisce il setInterval dell'orologio
+    this.startClock();
+  },
+  created(){
+    // azioni che vogliamo scatenare all'inizializzazione delle options
+    console.log('APP CREATA')
+  },
 
 }).mount('#app') // #app è il div che contiene l'app vue
